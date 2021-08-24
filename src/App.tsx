b958910dea377f13754ce9a15d3abac24a6a4416
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2021-08-24 04:35:44 +0300

    added onClick to onOff button

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './App.module.css';
 import {Rating} from "./components/Rating/Rating";
 import {Accordion} from "./components/Accordion/Accordion";
 import {OnOff} from "./components/OnOff/OnOff";
 
 function App() {
+    const [on, setOn] = useState(false);
+
+    const setOnOff = (value: boolean) => {
+        setOn(value);
+    }
+
     return (
         <div className={styles.App}>
             <Rating/>
             <Accordion
                 titleValue={'Menu'}
+
             />
-            <OnOff on={true}/>
+            <OnOff
+                on={on}
+                setOnOff={setOnOff}
+            />
         </div>
     );
 }
--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -2,27 +2,42 @@ import React from "react";
 
 type OnOffType = {
     on: boolean
+    setOnOff: (value: boolean) => void
 }
 
-const OnOff: React.FC<OnOffType> = (props)=>{
-    const onStyle ={
+const OnOff: React.FC<OnOffType> = (props) => {
+    const onStyle = {
         width: "30px",
-        height: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px",
+        backgroundColor: props.on ? "green" : "white"
     }
+
     const offStyle = {
         width: "30px",
-        height: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px",
+        backgroundColor: props.on ? "white" : "red"
     }
+
     const indicatorStyle = {
         width: "10px",
         height: "10px",
         borderRadius: "5px",
-        border: "1px solid black"
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "5px",
+        backgroundColor: props.on ? "green" : "red"
     }
-    return(
+    
+    return (
         <div>
-            <div></div>
-            <div></div>
+            <div onClick={()=>{props.setOnOff(true)}} style={onStyle}>On</div>
+            <div onClick={()=>{props.setOnOff(false)}} style={offStyle}>Off</div>
             <div style={indicatorStyle}></div>
         </div>
     )
